c6f78e577c8a969d6163cc98771929e322c1b3c9
Snehasaini16 <149758019+Snehasaini16@users.noreply.github.com>
2026-08-29 10:22:36 +0000

    conversion of datatypes

---
+++ .devcontainer/Basics/Variables.js
@@ -0,0 +1,14 @@
+const accountId = 34848 // val is fixed 
+let accountEmaill = "sneha@gmail.com" //
+var accountPass = "idk"
+accountCity = "Jaipur" //bad practice
+let accountState //stores val as undefined
+
+//accountId = 2 --> not allowed
+accountEmail = "hehhe"
+accountPass="jfjj"
+accountCity="bengaluru"
+
+console.table([accountEmail,accountPass,accountCity,accountState])
+/* prefer not use var bcoz of issue is block scope
+ and fnal scope*/
---
+++ .devcontainer/Basics/datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treats all JS code as newer version
+
+//alert(3+3) --> we are using node js not browser there is diff syntax to use alert in node
+
+//avoid semicolons
+
+//DATATYPE CONVERSION AND OPERATIONS
+// let score = "33abc" //if score=NULL its value will be 0
+//similary u can check for undefined,bool val(t/f)
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let val = Number(score)
+// console.log(typeof val)
+// console.log(val)
+
+// 33 -> 33
+// true->1 , false->0
+
+let isLoggedIn = "eejejeje"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1->true , 0->false,""->false,"sneha"->true
+let someNum = 12
+
+let stringNum = String(someNum)
+console.log(typeof stringNum)
+
