8a883973c80e77634482d15ab02ea41c7df57cd6
Snehasaini16 <149758019+Snehasaini16@users.noreply.github.com>
2026-09-09 18:28:13 +0000

    learning loops

---
+++ .devcontainer/controlflow/loops.js
@@ -0,0 +1,16 @@
+// FOR LOOP
+
+// for(let index =0; index < 10;index++){
+//     const elmnt = index
+//     console.log(elmnt);
+// }
+
+//Do While loop
+let score =11
+
+do{
+    console.log(`Score is ${score}`);
+    score++
+    
+} while(score<=10);
+
