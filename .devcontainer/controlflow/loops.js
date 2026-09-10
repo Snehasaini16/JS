d86bfb7b3a48168d328108fcc819168152f4b496
Snehasaini16 <149758019+Snehasaini16@users.noreply.github.com>
2026-09-10 14:19:12 +0000

    still learning loops

--- .devcontainer/controlflow/loops.js
+++ .devcontainer/controlflow/loops.js
@@ -5,12 +5,51 @@
 //     console.log(elmnt);
 // }
 
-//Do While loop
-let score =11
+//****Do While loop****
+// let score =11
 
-do{
-    console.log(`Score is ${score}`);
-    score++
+// do{
+//     console.log(`Score is ${score}`);
+//     score++
     
-} while(score<=10);
+// } while(score<=10);
+
+//***** for of *****//
+
+// const arr = [1,2,3,4,5]
+
+// for (const i of arr) {
+//     console.log(i)
+// }
+
+// const greet = "hello"
+// for(const i of greet){
+//     console.log(`Each char is ${i}`)
+// }
+
+// ******MAPS******* => stores unique vals and order is same
+
+// const map = new Map()
+// map.set('IN',"INDIA")
+// map.set('USA',"United States of america")
+// map.set('fr',"France")
+
+// console.log(map)
+
+// for (const key of map) {
+//     console.log(key)
+// }
+
+// for (const [key,value] of map) {
+//     console.log(key, ':-', value)
+// }
+
+// const myObj = {
+//     'g1': 'NFS',
+//     'g2': 'spidey'
+// }
+
+// for (const [key,val] of myObj) {   =>not iteratable
+//     console.log(key, ':-', val)      othr ways to iterate
+// }
 
